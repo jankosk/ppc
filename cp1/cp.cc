3447523236644396224d8a24e8e7205f5f9acf0e
jan <jan.koskinen94@gmail.com>
2023-03-12 21:16:46 +0200

    Add correlated pairs basline solution

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -1,3 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+
+void print_array(double *arr, int ny, int nx) {
+    for (int y = 0; y < ny; ++y) {
+        for (int x = 0; x < nx; ++x) {
+            std::cout << arr[y * nx + x] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -7,7 +20,37 @@ This is the function you need to implement. Quick reference:
 - only parts with 0 <= j <= i < ny need to be filled
 */
 void correlate(int ny, int nx, const float *data, float *result) {
-    for (int j; j < j; ++j) {
-        continue;
+    double *normalized = new double[ny * nx];
+    for (int y = 0; y < ny; ++y) {
+        double mean = 0;
+        for (int x = 0; x < nx; ++x) {
+            mean += data[x + y * nx];
+        }
+        mean = mean / nx;
+        double stdiv = 0;
+        for (int x = 0; x < nx; ++x) {
+            normalized[x + y * nx] = data[x + y * nx] - mean;
+            stdiv += pow(data[x + y * nx] - mean, 2);
+        }
+        stdiv = sqrt(stdiv);
+        for (int x = 0; x < nx; ++x) {
+            int idx = x + y * nx;
+            normalized[idx] = normalized[idx] / stdiv;
+        }
     }
+    // print_array(normalized, ny, nx);
+    for (int i = 0; i < ny; ++i) {
+        for (int j = 0; j < ny; ++j) {
+            if (j < i) {
+                result[i + j * ny] = result[j + i * ny];
+                continue;
+            }
+            double sum = 0;
+            for (int x = 0; x < nx; ++x) {
+                sum += normalized[x + i * nx] * normalized[x + j * nx];
+            }
+            result[i + j * ny] = sum;
+        }
+    }
+    delete[] normalized;
 }
