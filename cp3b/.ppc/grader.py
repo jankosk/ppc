fe149a12fdf634306c5966b323fdf9ac6949172b
jan <jan.koskinen94@gmail.com>
2024-11-28 08:38:45 +0200

    Add .gitignore

--- cp3b/.ppc/grader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from ppcgrader.cli import cli
-import ppccp
-
-if __name__ == "__main__":
-    cli(ppccp.Config(single_precision=True, openmp=True))
--- mf1/.ppc/grader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from ppcgrader.cli import cli
-import ppcmf
-
-if __name__ == "__main__":
-    cli(ppcmf.Config(False))
